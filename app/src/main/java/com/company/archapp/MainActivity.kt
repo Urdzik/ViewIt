da58837790be0d3ff1f58923c7e49d114e2eba0c
Urdzik <Urdziksv@gmail.com>
2019-03-07 12:13:54 +0200

    Created file for Landmark Recognition.

--- app/src/main/java/com/company/archapp/MainActivity.kt
+++ app/src/main/java/com/company/archapp/MainActivity.kt
@@ -2,11 +2,19 @@ package com.company.archapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.cloud.FirebaseVisionCloudDetectorOptions
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+    fun buildCloudVisionOptions() {
+        val options = FirebaseVisionCloudDetectorOptions.Builder()
+            .setModelType(FirebaseVisionCloudDetectorOptions.LATEST_MODEL)
+            .setMaxResults(15)
+            .build()
     }
 }
+
---
+++ app/src/main/java/com/company/archapp/VisionImage.kt
@@ -0,0 +1,16 @@
+package com.company.archapp
+
+
+import android.graphics.Bitmap
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+
+
+class VisionImage {
+    private fun imageFromBitmap(bitmap: Bitmap) {
+        // [START image_from_bitmap]
+        val image = FirebaseVisionImage.fromBitmap(bitmap)
+        // [END image_from_bitmap]
+    }
+
+
+}
