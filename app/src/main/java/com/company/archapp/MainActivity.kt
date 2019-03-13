cb1689533dbf149d600d3888194a15a04e1fc73c
Антон <aantonssizov@gmail.com>
2019-03-13 23:49:35 +0200

    add sliding layout and come sleep

--- app/src/main/java/com/company/archapp/MainActivity.kt
+++ app/src/main/java/com/company/archapp/MainActivity.kt
@@ -1,81 +1,11 @@
 package com.company.archapp
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
-import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.ImageView
-import android.widget.LinearLayout
 
-class MainActivity : AppCompatActivity(), DetailFragment.OnSwipedLineListener {
-    private lateinit var imageView: ImageView
-    private lateinit var frameLayout: FrameLayout
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Add DetailFragment
-        addFragment(R.id.fragment_container, DetailFragment.newInstance(), false, false)
-
-        // Init views
-        imageView = findViewById<ImageView>(R.id.image_view)
-        frameLayout = findViewById(R.id.fragment_container)
-    }
-
-    /*
-    Method for adding fragments to activity
-     */
-    @SuppressLint("CommitTransaction")
-    protected fun addFragment(
-        containerRes: Int?,
-        fragment: Fragment,
-        addToBackStack: Boolean,
-        animate: Boolean
-    ) {
-        var transaction = this
-            .supportFragmentManager.beginTransaction()
-            .replace(
-                containerRes!!, fragment,
-                fragment.javaClass.simpleName
-            )
-        if (addToBackStack) {
-            transaction = transaction.addToBackStack(
-                fragment.javaClass.simpleName
-            )
-        }
-        if (animate) {
-            transaction = transaction.setTransition(
-                FragmentTransaction.TRANSIT_FRAGMENT_OPEN
-            )
-        }
-        transaction.commitAllowingStateLoss()
-    }
-
-    /**
-     * The method call when line of DetailFragment is swiped
-     */
-    override fun onLineSwiped(imageWeight: Float?, fragmentWeight: Float?) {
-        /*
-        Here, we change the weight of Image and DetailFragment
-         */
-        // We need to use LinearLayout.LayoutParams class
-        val imageParams = LinearLayout.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            0,
-            imageWeight!!
-        )
-        val fragmentParams = LinearLayout.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            0,
-            fragmentWeight!!
-        )
-
-        // set layout params to views
-        imageView.layoutParams = imageParams
-        frameLayout.layoutParams = fragmentParams
     }
 }
