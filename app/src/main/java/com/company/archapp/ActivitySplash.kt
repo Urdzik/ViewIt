1f752cded683047b8f7e474f0a0bea9f4178ebd6
urdzi <urdziksv@gmail.com>
2019-04-18 17:01:03 +0300

    Design Welcome Activity and Splash Activity

---
+++ app/src/main/java/com/company/archapp/ActivitySplash.kt
@@ -0,0 +1,38 @@
+package com.company.archapp
+
+import android.content.Intent
+import android.graphics.Typeface
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+
+class ActivitySplash : AppCompatActivity() {
+    private var textView: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        textView = findViewById(R.id.tv)
+
+        val typeface = Typeface.createFromAsset(assets, "fonts/ProductSans-Bold.ttf")
+        textView!!.typeface = typeface
+
+        //Code to start timer and take action after the timer ends
+        Handler().postDelayed({
+            //Do any action here. Now we are moving to next page
+            val mySuperIntent = Intent(this@ActivitySplash, WelcomeActivity::class.java)
+            startActivity(mySuperIntent)
+            /* This 'finish()' is for exiting the app when back button pressed
+                 *  from Home page which is ActivityHome
+                 */
+            finish()
+        }, SPLASH_TIME.toLong())
+    }
+
+    companion object {
+
+        private val SPLASH_TIME = 2000 //This is 4 seconds
+    }
+}
